837f42a5175dff74bf5b7078eae5818cd520bd62
charlyschulte <dddbernhard@yahoo.de>
2022-10-26 16:52:20 +0200

    added callback

--- src/server.js
+++ src/server.js
@@ -1,11 +1,43 @@
-const { addFog,getCurrentFogCueCount } = require('./fog');
-const express = require('express')
-const{log} = require('./log'); 
-const app = express()
-app.get('/fog', (req, res) => { addFog(); res.send('Fog Added, current Cue: '+getCurrentFogCueCount()) })
-function runApi(){
-    app.listen(3000, () => { 
-        log('Server is up!') 
-    })
+const { addFog, getCurrentFogCueCount } = require("./fog");
+const express = require("express");
+const parsedJSON = require("../settings.json");
+const { log } = require("./log");
+const axios = require("axios");
+const app = express();
+const apicaller = axios.create({
+  timeout: 1000,
+});
+app.get("/fog", (req, res) => {
+  addFog();
+  callApi(getCurrentFogCueCount());
+  res.send("Fog Added, current Cue: " + getCurrentFogCueCount());
+});
+function callApi(cueCount) {
+  if (
+    parsedJSON.callback !== undefined &&
+    parsedJSON.callback.url !== undefined &&
+    parsedJSON.callback.param !== undefined
+  ) {
+    apicaller
+      .get(
+        parsedJSON.callback.url +
+          "?" +
+          parsedJSON.callback.param +
+          "=" +
+          cueCount
+      )
+      .then((response) => {
+        console.log(response.data.url);
+        console.log(response.data.explanation);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
-exports.runApi=runApi;
+function runApi() {
+  app.listen(3000, () => {
+    log("Server is up!");
+  });
+}
+exports.runApi = runApi;
